75999e65995e9e923c63124f25412fd1b0d1bc05
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-04-09 23:59:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+   <h2 className='font-bold text-xl'>hello</h2>
+    </div>
+  )
+}
+
+export default App
